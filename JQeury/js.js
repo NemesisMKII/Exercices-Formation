dec4ee090e5cf214c121e1655872a2f909f34b11
Bernard Leborgne <leborgne.bernard62@gmail.com>
2021-01-19 16:24:56 +0100

    AAAAAAH JQUERY

--- JQeury/js.js
+++ JQeury/js.js
@@ -16,6 +16,7 @@
     $(`#form-button`).click((e) => {
         e.preventDefault()
     })
+    
     //===============================================//
     //Ex 3
     var champ = $(`#mdp`)
@@ -32,14 +33,56 @@
     
     //===============================================//
     //Ex 4
+
+    table = {
+        "tab0": 5,
+        "tab1": "hey"
+    }
+
     var tabinput = "";
-    var inser = $("#tabinput");
-    $(valsubmit).click((e) => {
-        e.preventDefault()
-        tabinput = inser
-        $("#table").append(`<tr><td> ${inser.val()} </td></tr>`)
-        inser.val("")
-    })
+    var firstdel = $("#firstdel")
+    var lastdel = $("#lastdel")
+    var localtable = Object.keys(table)
+    var tab = $("#table")
+    main()
+    
+    function main() {
+        if (!localStorage.getItem("tableau")) {
+            localStorage.setItem("tableau", JSON.stringify(localtable))
+        } else {
+            localtable = JSON.parse(localStorage.getItem("tableau"))
+        }
+    
+        for (i in Object.keys(localtable)) {
+            tab.append(`<tr><td> ${localtable[`tab${i}`]} </td></tr>`)
+        }
+        
+        $("#valsubmit").click((e) => {
+            e.preventDefault()
+            tab.append(`<tr><td> ${$("#tabinput").val()} </td></tr>`)
+            localtable[`tab${tab.children("tr").length}`] = $("#tabinput").val()
+            $("#tabinput").val("")
+            JSONsave()
+        })
+    
+        firstdel.click(() => {ajoutsupp("first")})
+        lastdel.click(() => {ajoutsupp("last")})
+    
+        function ajoutsupp(delsup) {
+            if (delsup == "first") {
+                tab.children("tr").first().remove()
+                localtable.shift()
+            } else {
+                tab.children("tr").last().remove()
+            }
+            JSONsave()
+        }
+
+        function JSONsave() {
+            localStorage.setItem("tableau", JSON.stringify(localtable))
+        }
+    }
+    
 
     //=============================//
     //Ex 5//
---
+++ JQeury/ticket.js
@@ -0,0 +1,83 @@
+$(document).ready(() => {
+    var produit;
+    var resultat = 0;
+
+    if (!localStorage.getItem("tableau")) {
+        produit = {
+            "produit": []
+        }
+    } else {
+        produit = JSON.parse(localStorage.getItem("tableau"))
+
+        for (x in produit.produit) {
+            var ligne = produit.produit[x]
+
+            $("#tabbody").append(`
+        <tr>
+            <td>${ligne.nom}</td>
+            <td>${ligne.quantite}</td>
+            <td>${ligne.prix}</td>
+            <td>${ligne.prix_total}</td>
+            <td><button class="btn btn-sm btn-danger delete">Supprimer</button></td>
+        </tr>`)
+        }
+    }
+    
+
+    var nom_produit = $("#nom_produit")
+    var quantite_produit = $("#quantite_produit")
+    var prix_produit = $("#prix_produit")
+
+    $(`#form-button`).click((e) => {
+        e.preventDefault()
+        var prix_total_produit = quantite_produit.val() * prix_produit.val()
+
+        $("#tabbody").append(`
+        <tr>
+            <td>${nom_produit.val()}</td>
+            <td>${quantite_produit.val()}</td>
+            <td>${prix_produit.val()}</td>
+            <td>${prix_total_produit}</td>
+            <td><button class="btn btn-sm btn-danger delete">Supprimer</button></td>
+        </tr>`)
+
+        var objet = {
+            nom: nom_produit.val(),
+            quantite: quantite_produit.val(),
+            prix: prix_produit.val(),
+            prix_total: prix_total_produit
+        }
+
+        produit.produit.push(objet)
+        localStorage.setItem("tableau", JSON.stringify(produit))
+
+        nom_produit.val("")
+        quantite_produit.val("")
+        prix_produit.val("")
+
+        $(`.delete`).unbind("click", deleteligne)
+        $(".delete").click(deleteligne)
+    })
+
+    $(`.delete`).click(deleteligne)
+
+    function deleteligne(event) {
+        var monButton = $(this)
+        var maligne = monButton.parent().parent()
+        resultat -= maligne.children(3).text()
+        console.log(maligne.children()[2].text());
+
+        for (x in produit.produit) {
+            if (maligne.children().first().text() == produit.produit[x].nom) {
+                maligne.remove()
+                produit.produit.splice(x, 1)
+                localStorage.setItem("tableau", JSON.stringify(produit))
+            }
+        }
+    }
+    for (x in produit.produit) {
+        resultat += produit.produit[x].prix_total
+        console.log(resultat);
+    }
+    $(`#resultat`).append(`Le prix est de: ${resultat}`)
+})
